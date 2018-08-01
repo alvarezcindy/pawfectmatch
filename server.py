c30f8ce9cacc7c10ca4c7824e359122b70b8c559
Cindy Alvarez <calvarez519@gmail.com>
2018-08-01 19:31:44 +0000

    Added a dropdown form to the homepage

--- server.py
+++ server.py
@@ -20,8 +20,24 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return render_template("homepage.html")
+    traits = db.session.query(Characteristic.name).all()
+    return render_template("homepage.html", traits=traits)
 
+@app.route('/dog-breeds', methods=['POST'])
+def dog_traits():
+    pos_trait1 = request.form.get("pos_trait1")
+    pos_trait2 = request.form.get("pos_trait2")
+    pos_trait3 = request.form.get("pos_trait3")
+    neg_trait1 = request.form.get("neg_trait1")
+    neg_trait2 = request.form.get("neg_trait2")
+    neg_trait3 = request.form.get("neg_trait3")
+
+    pos_traits = (pos_trait1, pos_trait2, pos_trait3)
+    neg_traits = (neg_trait1, neg_trait2, neg_trait3)
+
+    return render_template("dogs_list.html",
+                            pos_traits=pos_traits, 
+                            neg_traits=neg_traits)
 
 if __name__ == "__main__":
 
