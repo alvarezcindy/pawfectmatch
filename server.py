1925cc5ced5d8f14e7d7684c0627e3c259233194
Cindy Alvarez <calvarez519@gmail.com>
2018-08-07 22:44:06 +0000

    Added AJAX to handle same page form-output

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 """Dog Matching."""
 
-from jinja2 import StrictUndefined
+import os
+import requests
 
-from flask import Flask, render_template, request, flash, redirect, session
+from jinja2 import StrictUndefined
+from flask import Flask, render_template, request, flash, redirect, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from pprint import pprint, pformat
 
 from model import connect_to_db, db, Breed, Rating, Characteristic
-
-import os
 
 #initialize Flask app
 app = Flask(__name__)
@@ -27,14 +28,14 @@ def index():
     traits = db.session.query(Characteristic.name).all()
     return render_template("homepage.html", traits=traits)
 
-@app.route('/dog-list', methods=['POST'])
+@app.route('/dog-list.json', methods=['POST'])
 def dog_traits():
     pos_trait1 = request.form.get("pos_trait1")
     pos_trait2 = request.form.get("pos_trait2")
     pos_trait3 = request.form.get("pos_trait3")
-    neg_trait1 = request.form.get("neg_trait1")
-    neg_trait2 = request.form.get("neg_trait2")
-    neg_trait3 = request.form.get("neg_trait3")
+    # neg_trait1 = request.form.get("neg_trait1")
+    # neg_trait2 = request.form.get("neg_trait2")
+    # neg_trait3 = request.form.get("neg_trait3")
 
     pos_traits = (pos_trait1, pos_trait2, pos_trait3)
 
@@ -47,6 +48,31 @@ def dog_traits():
                               ((Rating.score==5) |
                                (Rating.score==4)))
                       .group_by(Breed.name))
+
+    payload = {'key': PETFINDER_KEY,
+               'animal': 'dog',
+               'breed': 'Chihuahua',
+               'location': '94702',
+               'count': 1,
+               'format': 'json'
+               }
+
+    data = requests.get(PETFINDER_URL + 'pet.find', params=payload)
+    data = data.json()
+
+    photos = data['petfinder']['pets']['pet']['media']['photos']['photo']
+
+    # for photo in photos:
+    #     if photo['@size'] != 'x':
+    #       del photo
+
+    #     {% for photo in photos %}
+    #     {% if photo['@size'] == 'x' %}
+    #         {{ photo['$t'] }}
+    #     {% endif %}
+    # {% endfor %}
+
+    # results = data['pets']
 
     # #SQL!
     # SELECT breeds.name
@@ -64,9 +90,12 @@ def dog_traits():
     # GROUP BY breeds.name
     # LIMIT 30;
 
-    return render_template("dog_list.html", 
-                           traits=pos_traits,
-                           dogs=dogs[0:20])
+    results = {'traits': pos_traits,
+               'dogs': dogs[0:20],
+               'photos': photos}
+
+    return render_template('dog_list.html', traits=pos_traits, photos=photos, dogs=dogs[0:20])
+    # return jsonify(results)
 
 if __name__ == "__main__":
 
--- static/js/breeds.js
+++ static/js/breeds.js
@@ -1,25 +1,21 @@
-// "use strict";
+"use strict";
 
-// function getDogBreeds(results) {
-//     console.dir(results);
-//     alert("This will cost $" + results['cost']);
-// }
+function getDogBreeds(results) {
+    let status = results;
+    $('#form-results').html('<p> hello </p>');
 
-// function getDogTraits(evt) {
-//     evt.preventDefault();
+function getDogTraits(evt) {
+    evt.preventDefault();
 
-//     var formInputs = {
-//         "pos_trait1": $("pos_trait1").val(),
-//         "pos_trait2": $("pos_trait2").val(),
-//         "pos_trait3": $("pos_trait3").val(),
-//         "neg_trait1": $("neg_trait1").val(),
-//         "neg_trait2": $("neg_trait2").val(),
-//         "neg_trait3": $("neg_trait3").val(),
-//     };
+    var formInputs = {
+        "pos_trait1": $("#pos_trait1").val(),
+        "pos_trait2": $("#pos_trait2").val(),
+        "pos_trait3": $("#pos_trait3").val(),
+    };
 
-//     $.get("/delivery-info.json",
-//         formInputs,
-//         getDogBreeds);
-// }
+    $.post("/dog-list.json",
+           formInputs,
+           getDogBreeds);
+}
 
-// $("#dog-traits-form").on("submit", getDogTraits);
+$("#dog-traits-form").on("submit", getDogTraits);
